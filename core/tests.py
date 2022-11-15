d19bb692eec3fdb4f8c258f3826035b11ba86982
Gregorio Honorato <greghono@gmail.com>
2022-11-15 13:28:03 -0300

    tests unitary

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestHidro(TestCase):
+    def test_router_query_return_should_ok(self):
+        self.asertTrue(True)
+
+    def test_router_register_return_should_ok(self):
+        self.asertTrue(True)
+    
+    def test_router_register_with_document_error_return_should_error(self):
+        self.asertFalse(False)                
+
+    def test_router_query_empty_return_should_ok(self):
+        self.assertAlmostEqual([], [])
